d1fcb1a5d1f0ee91ac57cd0694b845829e4595a2
Samil-bunga <125383988+Samil-bunga@users.noreply.github.com>
2026-07-10 03:05:16 +0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,38 @@
+/*
+==================================
+Aurora Birthday Website
+UI Controller
+==================================
+*/
 
+const loadingScreen = document.getElementById("loading-screen");
+const app = document.getElementById("app");
+const startButton = document.getElementById("startButton");
+
+function showMainScreen() {
+
+    loadingScreen.classList.add("hide");
+
+    setTimeout(() => {
+
+        app.classList.add("show");
+
+    }, 500);
+
+}
+
+function setupUI() {
+
+    startButton.addEventListener("click", () => {
+
+        console.log("START clicked");
+
+        showMainScreen();
+
+        if (typeof startMusic === "function") {
+            startMusic();
+        }
+
+    });
+
+}
