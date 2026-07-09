844bdc0aa80902d85b86b9fea38d8deef6cd4bf2
Samil-bunga <125383988+Samil-bunga@users.noreply.github.com>
2026-07-10 03:06:18 +0700

    Update music.js

--- js/music.js
+++ js/music.js
@@ -1,1 +1,23 @@
+/*
+==================================
+Music Controller
+==================================
+*/
 
+const music = document.getElementById("birthdayMusic");
+
+function startMusic() {
+
+    if (!music) return;
+
+    music.src = CONFIG.music;
+
+    music.volume = CONFIG.musicVolume;
+
+    music.play().catch(() => {
+
+        console.log("Music belum tersedia.");
+
+    });
+
+}
