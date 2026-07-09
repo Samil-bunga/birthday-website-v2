133a207af6e9df0d5b7b12527bf631f5d7292856
Samil-bunga <125383988+Samil-bunga@users.noreply.github.com>
2026-07-10 03:06:44 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
+/*
+==================================
+Main App
+==================================
+*/
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("title").textContent = CONFIG.title;
+
+    document.getElementById("subtitle").textContent = CONFIG.subtitle;
+
+    document.getElementById("message").innerText = CONFIG.message;
+
+    setupUI();
+
+});
